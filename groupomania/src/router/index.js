937ca3272f3fe2ccf2a4d0295e678b78d72a1261
Lydie Janovsky <lydiejanovsky@hotmail.fr>
2022-01-24 00:29:04 +0100

    Modifications intermédiaires

--- groupomania/src/router/index.js
+++ groupomania/src/router/index.js
@@ -1,26 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Signup from '../views/Signup.vue'
+import Login from "../views/Login.vue";
+import Profile from "../views/Profile.vue";
 
 Vue.use(VueRouter)
 
-const routes = [
+const routes = [{
+  name: 'Signup',
+  path: '/signup',
+  component: Signup,
+},
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Login',
+    path: '/login',
+    component: Login
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+{
+  name: 'Profile',
+  path: '/profile',
+  component: Profile,
+  props: true
+},
 ]
 
+
+
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
---
+++ groupomania/src/views/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <NavProfile />
+    </div>
+
+    <p>hello</p>
+  </div>
+</template>
+
+<script>
+import NavProfile from "../components/NavProfile";
+export default {
+  name: "Profile",
+  components: {
+    NavProfile,
+  },
+};
+</script>
+
+<style scoped>
